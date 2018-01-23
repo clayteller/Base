ba33b2161fbba9bcb51f7e9a21616cc98a6ade9e
Clay Teller <email@clayteller.com>
2018-01-23 17:03:54 -0600

    Two-column layout for 'aside' using css grid. Revise full-width buttons and section spacing

--- wp-content/themes/base/sidebar.php
+++ wp-content/themes/base/sidebar.php
@@ -13,5 +13,7 @@ if ( ! is_active_sidebar( 'site-aside' ) ) return;
 ?>
 
 <aside id="site-aside" class="site-aside">
-	<?php dynamic_sidebar( 'site-aside' ); ?>
+	<div class="wrap">
+		<?php dynamic_sidebar( 'site-aside' ); ?>
+	</div>
 </aside><!-- #site-aside -->
