5cdad01698a0e61b220b951abf5cf50315478fed
Clay Teller <email@clayteller.com>
2018-02-19 13:06:43 -0600

    Fix gulp image processing for progressive jpgs

--- wp-content/themes/base/gulpfile.js
+++ wp-content/themes/base/gulpfile.js
@@ -1,34 +1,35 @@
-// Gulp packages
-var gulp        = require( 'gulp' ),
-	autoprefixer = require( 'autoprefixer' ),
-	browsersync  = require( 'browser-sync' ),
-	changed      = require( 'gulp-changed' ),
-	cssnano      = require( 'gulp-cssnano' ),
+var
+	// Gulp packages
+	gulp           = require( 'gulp' ),
+	autoprefixer   = require( 'autoprefixer' ),
+	browsersync    = require( 'browser-sync' ),
+	changed        = require( 'gulp-changed' ),
+	cssnano        = require( 'gulp-cssnano' ),
 	// cssnext seems to be duplicating some of the css rules, so I'm commenting it out for now
-	// cssnext      = require( 'postcss-cssnext' ),
-	gutil        = require( 'gulp-util' ),
-	ignore       = require( 'gulp-ignore' ),
-	imagemin     = require( 'gulp-imagemin' ),
-	map          = require( 'gulp-sourcemaps' ),
-	math         = require( 'postcss-math' ),
-	plumber      = require( 'gulp-plumber' ),
-	pngquant     = require( 'imagemin-pngquant' ),
-	postcss      = require( 'gulp-postcss' ),
-	precss       = require( 'precss' ),
-	rename       = require( 'gulp-rename' ),
+	// cssnext     = require( 'postcss-cssnext' ),
+	gutil          = require( 'gulp-util' ),
+	ignore         = require( 'gulp-ignore' ),
+	imagemin       = require( 'gulp-imagemin' ),
+	map            = require( 'gulp-sourcemaps' ),
+	math           = require( 'postcss-math' ),
+	plumber        = require( 'gulp-plumber' ),
+	pngquant       = require( 'imagemin-pngquant' ),
+	postcss        = require( 'gulp-postcss' ),
+	precss         = require( 'precss' ),
+	rename         = require( 'gulp-rename' ),
 	responsivetype = require( 'postcss-responsive-type' ),
-	uglify       = require( 'gulp-uglify' );
+	uglify         = require( 'gulp-uglify' ),
 
-// Source and destination directories
-var jsSource  = 'source/js/**/*.js',
-	jsDest     = 'js',
-	cssSource  = 'source/**/[^_]*.css',
-	cssSourceWatch  = 'source/**/*.css',
-	cssDest    = './',
-	imgSource  = 'source/images/**/*',
-	imgDest    = 'images',
-	iconSource = 'source/icons/**/*',
-	iconDest   = 'icons';
+	// Source and destination directories
+	jsSource       = 'source/js/**/*.js',
+	jsDest         = 'js',
+	cssSource      = 'source/**/[^_]*.css',
+	cssSourceWatch = 'source/**/*.css',
+	cssDest        = './',
+	imgSource      = 'source/images/**/*',
+	imgDest        = 'images',
+	iconSource     = 'source/icons/**/*',
+	iconDest       = 'icons';
 
 var onError = function ( err ) {
 		gutil.beep( 2 );
@@ -75,22 +76,33 @@ gulp.task( 'css', function() {
 
 // Images
 gulp.task( 'images', function () {
-	// SVG is not working correctly so just optimize JPG and PNG
-	gulp.src( imgSource + '.{jpg,jpeg,png}' )
-		.pipe( imagemin( {
-			progressive: true,
-			// svgoPlugins: [{removeViewBox: false}],
-			use: [ pngquant() ]
-		}))
-		.pipe( gulp.dest( imgDest ) );
-	// Just copy SVGs without optimizing for now
-	gulp.src( imgSource + '.svg' )
+	gulp.src( imgSource )
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeDimensions: true},
+					{removeViewBox:    false}
+				]
+			})
+		]))
 		.pipe( gulp.dest( imgDest ) );
 } );
 
 // Icons
+// @todo figure out how to "combine" Images and Icons tasks
 gulp.task( 'icons', function () {
 	gulp.src( iconSource )
+		.pipe(imagemin([
+			imagemin.svgo({
+				plugins: [
+					{removeDimensions: true},
+					{removeViewBox:    false}
+				]
+			})
+		]))
 		.pipe( gulp.dest( iconDest ) );
 } );
 
