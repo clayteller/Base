3c008a3221cdee9b2cb94fd98d2fa37702ae6d32
Clay Teller <email@clayteller.com>
2018-03-18 13:43:15 -0500

    Misc cleanup

--- wp-content/plugins/base-admin/inc/theme-settings.php
+++ wp-content/plugins/base-admin/inc/theme-settings.php
@@ -11,7 +11,7 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_page( array(
 		'page_title' 	=> 'Base Theme Settings',
 		'menu_title'	=> 'Base Theme',
-		'menu_slug' 	=> 'starter-theme-settings',
+		'menu_slug' 	=> 'base-theme-settings',
 		'capability'	=> 'edit_pages',
 		'position'	   => 3,
 		'icon_url'     => 'dashicons-admin-site'
@@ -20,31 +20,31 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_sub_page( array(
 		'page_title' 	=> 'Site Header',
 		'menu_title'	=> 'Site Header',
-		'parent_slug'	=> 'starter-theme-settings'
+		'parent_slug'	=> 'base-theme-settings'
 	));
 
 	acf_add_options_sub_page( array(
 		'page_title' 	=> 'Site Footer',
 		'menu_title'	=> 'Site Footer',
-		'parent_slug'	=> 'starter-theme-settings',
+		'parent_slug'	=> 'base-theme-settings',
 	));
 
 	acf_add_options_sub_page( array(
 		'page_title' 	=> 'Company Contact',
 		'menu_title'	=> 'Company Contact',
-		'parent_slug'	=> 'starter-theme-settings',
+		'parent_slug'	=> 'base-theme-settings',
 	));
 
 	acf_add_options_sub_page( array(
 		'page_title' 	=> 'Subscribe Form',
 		'menu_title'	=> 'Subscribe Form',
-		'parent_slug'	=> 'starter-theme-settings',
+		'parent_slug'	=> 'base-theme-settings',
 	));
 
 	acf_add_options_sub_page( array(
 		'page_title' 	=> 'Partner Logos',
 		'menu_title'	=> 'Partner Logos',
-		'parent_slug'	=> 'starter-theme-settings',
+		'parent_slug'	=> 'base-theme-settings',
 	));
 
 }
