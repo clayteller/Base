6bd7e0096038b8a969d7d793b2e4f4790a8ea06d
Clay Teller <email@clayteller.com>
2021-04-10 15:40:01 -0500

    Update all the gulp packages. The 'precss' package hasn't been updated in years and doesn't work well with new 'postcss' package. Led to me removing 'precss' and reproducing the functionality with several individual packages. Had to change some of the postcss function names used in css files as a result. During this process I tried to change css variables from postcss variables to native css variables (custom properties). Caused too many issues with some of the postcss functions so I had to stick with postcss variables.
    Move assets into 'assets' directory. Pull out sections of main stylesheet into their own stylesheet.

--- wp-content/themes/base/gulpfile.js
+++ wp-content/themes/base/gulpfile.js
@@ -1,155 +1,180 @@
-var
-	// Gulp packages
-	gulp           = require( 'gulp' ),
-	autoprefixer   = require( 'autoprefixer' ),
-	browsersync    = require( 'browser-sync' ),
-	changed        = require( 'gulp-changed' ),
-	cssnano        = require( 'gulp-cssnano' ),
-	// cssnext seems to be duplicating some of the css rules, so I'm commenting it out for now
-	// cssnext     = require( 'postcss-cssnext' ),
-	gutil          = require( 'gulp-util' ),
-	ignore         = require( 'gulp-ignore' ),
-	imagemin       = require( 'gulp-imagemin' ),
-	map            = require( 'gulp-sourcemaps' ),
-	math           = require( 'postcss-math' ),
-	plumber        = require( 'gulp-plumber' ),
-	pngquant       = require( 'imagemin-pngquant' ),
-	postcss        = require( 'gulp-postcss' ),
-	precss         = require( 'precss' ),
-	rename         = require( 'gulp-rename' ),
-	responsivetype = require( 'postcss-responsive-type' ),
-	uglify         = require( 'gulp-uglify' ),
-	wpPot          = require( 'gulp-wp-pot' ),
+const
+	// Import Gulp
+	{ src, dest, watch, parallel, series, lastRun } = require( 'gulp' ),
 
-	// Source and destination directories
-	cssDest        = './',
-	cssSource      = 'source/**/[^_]*.css',
-	cssSourceWatch = 'source/**/*.css',
-	iconDest       = 'icons',
-	iconSource     = 'source/icons/**/*',
-	imgDest        = 'images',
-	imgSource      = 'source/images/**/*',
-	jsDest         = 'js',
-	jsSource       = 'source/js/**/*.js',
-	phpSource      = [
-		'./*.php',
-		'inc/**/*.php',
-		'template-parts/**/*.php',
-		],
+	// Import modules
+	autoprefixer = require( 'autoprefixer' ),
+	browsersync  = require( 'browser-sync' ),
+	cssnano      = require( 'gulp-cssnano' ),
+	gutil        = require( 'gulp-util' ),
+	ignore       = require( 'gulp-ignore' ),
+	imagemin     = require( 'gulp-imagemin' ),
+	localization = require( 'gulp-wp-pot' ),
+	map          = require( 'gulp-sourcemaps' ),
+	plumber      = require( 'gulp-plumber' ),
+	postcss      = require( 'gulp-postcss' ),
+	pcssadv      = require( 'postcss-advanced-variables' ),
+	pcsscalc     = require( 'postcss-calc' ),
+	pcsscolor    = require( 'postcss-color-function' ),
+	pcssnested   = require( 'postcss-nested' ),
+	pcsstype     = require( 'postcss-responsive-type' ),
+	rename       = require( 'gulp-rename' ),
+	terser       = require( 'gulp-terser' ),
 
-	onError = function ( err ) {
-		gutil.beep( 2 );
-		gutil.log( gutil.colors.white( err ) );
+	// Source and destination paths
+	paths = {
+		js: {
+			src:  'source/js/**/*.js',
+			dest: 'assets/js',
+		},
+		css: {
+			src:  'source/css/**/*.css',
+			dest: 'assets/css',
+		},
+		images: {
+			src:  'source/icons/**/*',
+			dest: 'assets/images',
+		},
+		icons: {
+			src:  'source/icons/**/*',
+			dest: 'assets/icons',
+		},
+		php: {
+			src:  [
+				'./*.php',
+				'inc/**/*.php',
+				'template-parts/**/*.php',
+			],
+		},
 	};
 
-// CSS
-gulp.task( 'css', function() {
-	var plugins = [
-		autoprefixer,
-		// cssnext,
-		precss,
-		math,
-		responsivetype
-	];
-	gulp.src( cssSource )
+const onError = function ( err ) {
+	gutil.beep( 2 );
+	gutil.log( gutil.colors.white( err ) );
+	this.emit( 'end' );
+};
+
+// JavaScript task
+function jsTask() {
+	return src( paths.js.src, { since: lastRun( jsTask ) } )
 		.pipe( plumber( {
-			errorHandler: onError
+			errorHandler: onError,
 		} ) )
-		.pipe( changed( cssDest ) )
+		.pipe( dest( paths.js.dest ) )
+		.pipe( terser() )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( dest( paths.js.dest ) );
+}
+
+// CSS task
+function cssTask() {
+	// PostCSS plugins
+	const plugins = [
+		pcssadv,
+		pcssnested,
+		pcsscalc,
+		pcsscolor,
+		pcsstype,
+		autoprefixer,
+	];
+	return src( paths.css.src )
+		.pipe( plumber( {
+			errorHandler: onError,
+		} ) )
 		.pipe( map.init() )
 		.pipe( postcss( plugins ) )
+		// After postcss processing we can ignore 'include' files
+		.pipe( ignore.exclude( 'inc/*' ) )
 		.pipe( map.write( '.' ) )
-		.pipe( gulp.dest( cssDest ) )
+		.pipe( dest( paths.css.dest ) )
 		.pipe( ignore.exclude( '*.map' ) )
 		.pipe( cssnano( { zindex: false } ) )
 		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( gulp.dest( cssDest ) )
-		.pipe( browsersync.stream() );
-} );
+		.pipe( dest( paths.css.dest ) );
+}
 
-// Icons
+// Images task
+function imagesTask() {
+	return src( paths.images.src, { since: lastRun( imagesTask ) } )
+		.pipe( imagemin( [
+			imagemin.gifsicle( { interlaced: true } ),
+			imagemin.mozjpeg( { progressive: true } ),
+			imagemin.optipng( { optimizationLevel: 5 } ),
+			imagemin.svgo( {
+				plugins: [
+					{ removeDimensions: true },
+					{ removeViewBox:    false },
+				],
+			} ),
+		] ) )
+		.pipe( dest( paths.images.dest ) );
+}
+
+// Icons task
 // @todo figure out how to "combine" Images and Icons tasks
-gulp.task( 'icons', function () {
-	gulp.src( iconSource )
-		.pipe(imagemin([
-			imagemin.svgo({
+function iconsTask() {
+	return src( paths.icons.src, { since: lastRun( iconsTask ) } )
+		.pipe( imagemin( [
+			imagemin.svgo( {
 				plugins: [
-					{convertPathData:    false},
-					{convertShapeToPath: false},
-					{removeDimensions:   true},
-					{removeViewBox:      false}
-				]
-			})
-		]))
-		.pipe( gulp.dest( iconDest ) );
-} );
+					{ cleanupIDs:         false },
+					{ convertPathData:    false },
+					{ convertShapeToPath: false },
+					{ removeDimensions:   true },
+					{ removeUselessDefs:  false },
+					{ removeViewBox:      false },
+				],
+			} ),
+		] ) )
+		.pipe( dest( paths.icons.dest ) );
+}
 
-// Images
-gulp.task( 'images', function () {
-	gulp.src( imgSource )
-		.pipe(imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.jpegtran({progressive: true}),
-			imagemin.optipng({optimizationLevel: 5}),
-			imagemin.svgo({
-				plugins: [
-					{removeDimensions: true},
-					{removeViewBox:    false}
-				]
-			})
-		]))
-		.pipe( gulp.dest( imgDest ) );
-} );
-
-// JavaScript
-gulp.task( 'js', function() {
-	gulp.src( jsSource )
+// WP Localization task
+function localizationTask() {
+	return src( paths.php.src )
 		.pipe( plumber( {
-			errorHandler: onError
+			errorHandler: onError,
 		} ) )
-		.pipe( changed( jsDest ) )
-		.pipe( gulp.dest( jsDest ) )
-		.pipe( uglify() )
-		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( gulp.dest( jsDest ) );
-} );
-
-// Translate
-gulp.task( 'translate', function() {
-	gulp.src( phpSource )
-		.pipe( plumber( {
-			errorHandler: onError
+		.pipe( localization( {
+			domain: 'base',
+			package: 'Base theme',
 		} ) )
-		.pipe( wpPot( {
-			 domain: 'base',
-			 package: 'Base theme'
-		} ) )
-		.pipe( gulp.dest( 'languages/base.pot' ) );
-} );
+		.pipe( dest( 'languages/base.pot' ) );
+}
 
-// Watch for changes
-gulp.task( 'watch', function() {
-	gulp.watch( cssSourceWatch, [ 'css' ] );
-	gulp.watch( iconSource, [ 'icons' ] );
-	gulp.watch( imgSource, [ 'images' ] );
-	gulp.watch( jsSource, [ 'js' ] );
-} );
+// Run tasks when files change
+function watchTask() {
+	// CSS changes
+	watch( paths.css.src, series( cssTask ) );
+	// JavaScript changes
+	watch( paths.js.src, series( jsTask ) );
+	// Images changes
+	watch( paths.images.src, series( imagesTask ) );
+	// Icons changes
+	watch( paths.icons.src, series( iconsTask ) );
+}
 
-// Reload browser when files change
-gulp.task( 'reload', function() {
-	var files = [
-		'style.css',
-		'css/**/*.css',
-		'js/**/*.js',
-		'images/**/*',
-		'icons/**/*',
-		'**/*.php'
+// Reload browser
+function syncBrowser() {
+	const files = [
+		'./assets/css/**/*.css',
+		'./assets/js/**/*.js',
+		'./assets/images/**/*',
+		'./assets/icons/**/*',
+		'./**/*.php',
 	];
 	browsersync.init( files, {
 		notify: false,
-		proxy: 'http://base.dev/'
+		proxy: 'http://base.dev/',
 	} );
-} );
+}
 
-// Default task
-gulp.task( 'default', [ 'css', 'icons', 'images', 'js', 'reload', 'translate', 'watch' ] );
+// Default gulp task
+exports.default = series( parallel( jsTask, cssTask, imagesTask, iconsTask ), parallel( watchTask, syncBrowser ) ); // $ gulp
+
+// Individual tasks
+exports.js           = jsTask; // $ gulp js
+exports.css          = cssTask; // $ gulp css
+exports.images       = imagesTask; // $ gulp images
+exports.icons        = iconsTask; // $ gulp icons
+exports.localization = localizationTask; // $ gulp localization
--- wp-content/themes/base/inc/theme-functions/scripts-styles.php
+++ wp-content/themes/base/inc/theme-functions/scripts-styles.php
@@ -10,13 +10,9 @@
  * Enqueue scripts and styles.
  */
 function base_scripts() {
-   // normalize.css
-	$file = base_get_min_file( '/css/normalize.min.css' );
-	wp_enqueue_style( 'base-normalize-css', THEME_URL . $file );
-
 	// style.css
-	$file = base_get_min_file( '/style.min.css' );
-	wp_enqueue_style( 'base-style', THEME_URL . $file, array( 'base-normalize-css' ) );
+	$file = base_get_min_file( '/assets/css/style.min.css' );
+	wp_enqueue_style( 'base-style', THEME_URL . $file );
 
    // Add inline css to display the "Header Background Image" (custom field)
    $header_background_image_css = base_get_background_image_css( 'site-header', 'header_background_image' );
@@ -31,13 +27,13 @@ function base_scripts() {
    // wp_add_inline_style( 'base-style', $background_image_css );
 
    // main.js
-	$file = base_get_min_file( '/js/main.min.js' );
+	$file = base_get_min_file( '/assets/js/main.min.js' );
 	wp_enqueue_script( 'base-main', THEME_URL . $file, array( 'jquery', 'base-gsap' ), false, true );
 
 	// lightbox.js (load on single post pages)
 	if ( is_singular()  ) {
 		// lightbox.js
-		$file = base_get_min_file( '/js/lightbox.min.js' );
+		$file = base_get_min_file( '/assets/js/lightbox.min.js' );
 		wp_enqueue_script( 'base-lightbox', THEME_URL . $file, array( 'jquery', 'base-featherlight', 'base-featherlight-gallery' ), false, true );
 
 		// Pass svg icon html to lightbox.js
@@ -47,21 +43,21 @@ function base_scripts() {
 		) );
 
 		// Featherlight css
-		$file = base_get_min_file( '/css/featherlight.min.css' );
+		$file = base_get_min_file( '/assets/css/featherlight.min.css' );
 		wp_enqueue_style( 'base-featherlight-style', THEME_URL . $file );
 		// Featherlight gallery css
-		$file = base_get_min_file( '/css/featherlight.gallery.min.css' );
+		$file = base_get_min_file( '/assets/css/featherlight.gallery.min.css' );
 		wp_enqueue_style( 'base-featherlight-gallery-style', THEME_URL . $file );
 
 		// Featherlight js
-		$file = base_get_min_file( '/js/lib/featherlight.min.js' );
+		$file = base_get_min_file( '/assets/js/lib/featherlight.min.js' );
 		wp_enqueue_script( 'base-featherlight', THEME_URL . $file, array( 'base-swipe' ), false, true );
 		// Featherlight gallery js
-		$file = base_get_min_file( '/js/lib/featherlight.gallery.min.js' );
+		$file = base_get_min_file( '/assets/js/lib/featherlight.gallery.min.js' );
 		wp_enqueue_script( 'base-featherlight-gallery', THEME_URL . $file, array( 'base-swipe' ), false, true );
 
 		// jQuery swipe
-		$file = base_get_min_file( '/js/lib/jquery.detect_swipe.min.js' );
+		$file = base_get_min_file( '/assets/js/lib/jquery.detect_swipe.min.js' );
 		wp_enqueue_script( 'base-swipe', THEME_URL . $file, array( 'jquery' ), '20151215', true );
 	}
 
